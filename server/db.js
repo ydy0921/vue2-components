c3af8de43a486fbbf5c50e3dc401144fa0cc31ea
kyrie890514 <kyrie890514@163.com>
2022-01-09 20:33:29 +0800

    init

---
+++ server/db.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mysql: {
+    host: 'localhost',
+    user: 'root',
+    password: 'kyrie890514,./',
+    database: 'people_select_tree',
+    port: '3306'
+  }
+}
---
+++ src/views/peopleSelectTree.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="people-select-tree">
+    people-select-tree
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'peopleSelectTree'
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
