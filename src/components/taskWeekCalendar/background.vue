af3af2187de9153fe8cfc6069716cafcefd18076
zhuhui_yq <kyrie890514@163.com>
2022-01-27 14:56:10 +0800

    taskWeekCalendar

---
+++ src/components/taskWeekCalendar/background.vue
@@ -0,0 +1,115 @@
+<template>
+  <div id="background">
+    <div class="header">
+      <div/>
+      <div v-for="i of 7" :key="i">
+        <div>{{ week[i - 1] }}</div>
+      </div>
+    </div>
+    <div class="content">
+      <div class="time-line-wrapper">
+        <div v-for="(v, i) in timeLine" :key="i"><span>{{ v }}</span></div>
+      </div>
+      <div class="row-line-wrapper">
+        <div v-for="i of 25" :key="i"/>
+      </div>
+      <div class="day-calendar-wrapper">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'background',
+  data () {
+    return {
+      week: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+      timeLine: ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00', '24:00']
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style scoped lang="scss">
+div, /deep/ div {
+  box-sizing: border-box;
+}
+
+#background {
+  display: flex;
+  flex-flow: column nowrap;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+
+  > div {
+    width: 100%;
+    background-clip: content-box !important;
+    padding-right: 10px;
+  }
+
+  .header {
+    height: 60px;
+    background: #f0f5ff;
+    display: flex;
+
+    > div:not(:first-child) {
+      height: 100%;
+      flex: 1;
+      border: 1px solid #e0e2e8;
+      border-left: none;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      > div:first-child {
+        font-weight: bold;
+        font-size: 18px;
+      }
+    }
+
+    > div:first-child {
+      width: 68px;
+      height: 100%;
+      border: 1px solid #e0e2e8;
+    }
+  }
+
+  .content {
+    flex: 1;
+    height: 100%;
+    overflow-y: overlay;
+    display: flex;
+    position: relative;
+
+    .time-line-wrapper {
+      height: 1500px;
+      width: 68px;
+      border-left: 1px solid #e0e2e8;
+      border-right: 1px solid #e0e2e8;
+
+      > div {
+        height: 60px;
+        border-bottom: 1px solid #e0e2e8;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: #f0f5ff;
+      }
+    }
+
+    .row-line-wrapper {
+      height: 1500px;
+      flex: 1;
+      border-right: 1px solid #e0e2e8;
+
+      > div {
+        border-bottom: 1px solid #e0e2e8;
+        height: 60px;
+      }
+    }
+  }
+}
+</style>
--- src/views/taskWeekCalendar.vue
+++ src/views/taskWeekCalendar.vue
@@ -1,16 +1,27 @@
 <template>
   <div id="taskWeekCalendar">
     <p>task-week-calendar</p>
-
+    <div class="task-week-calendar-wrapper">
+      <background/>
+    </div>
   </div>
 </template>
 
 <script>
+import background from '../components/taskWeekCalendar/background'
+
 export default {
-  name: 'taskWeekCalendar'
+  name: 'taskWeekCalendar',
+  components: {
+    background
+  }
 }
 </script>
 
 <style scoped lang="scss">
-
+.task-week-calendar-wrapper {
+  width: 80%;
+  height: 800px;
+  margin: 50px auto;
+}
 </style>
